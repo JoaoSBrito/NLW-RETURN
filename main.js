1b3cc5f8b8b959dc2235e574e218d3b3ebdf0464
Joao Vitor Brito <joaovbrito123@gmail.com>
2022-05-04 14:27:18 -0300

    second commit

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px'
+}).reveal(`#home, 
+#home img,
+#home .stats,
+#services,
+#services header,
+#services.card,
+#about,
+#about header, 
+#about .content`)
